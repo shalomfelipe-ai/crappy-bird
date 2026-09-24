3b53eff1d63fcd08a7f4f7bdf1253e2c375d2422
Felipe <philipp.bezerra@gmail.com>
2026-09-24 23:16:45 +0200

    build web de 2026-09-24 23:16:45.71

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790273285|10831125';
+const CACHE_VERSION = '1790284601|5412813';
 /** @type {string} */
 const CACHE_PREFIX = 'Crappy Bird-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
